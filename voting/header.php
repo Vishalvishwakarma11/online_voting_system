595c3e6108d478f795a7c5644383d84cc653ce7c
Vishal A Vishwakarma <vishal.vish6232@gmail.com>
2022-06-09 21:56:26 +0530

    Add files via upload

---
+++ voting/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Online Voting System</title>
+<script src="jscript/validation.js" type="text/javascript"></script>
+<style>
+
+    body,html{
+        height: 100%;
+    }
+    body{
+        height: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+</head>
+<body style="background-image: url('img/bg.jpg'); ">
+<marquee>Welcome To Online Voting System Coded By MARUF</marquee>
+<center><font size='8' >
+<a href="index.php">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="register.php">Register</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="login.php">Login</a></font></center> 
+<br>
+<br>
