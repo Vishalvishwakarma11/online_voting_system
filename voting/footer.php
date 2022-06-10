1ce736add3728ece5b7f9195c5d3a7e6b555d675
Vishal A Vishwakarma <vishal.vish6232@gmail.com>
2022-06-10 10:48:11 +0530

    Update footer.php

--- voting/footer.php
+++ voting/footer.php
@@ -2,6 +2,6 @@
 <br>
 <br>
 <br>
-<center><font color="red"><b><i>Declaration: </i></b></font><i>Madebymaruf&nbsp;</i></center>
+<center><font color="red"><b><i>Declaration: </i></b></font><i>CODEWITHVISH&nbsp;</i></center>
 </body>
 </html>
